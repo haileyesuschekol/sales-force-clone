5621f77d27655360cc347497e1e17506051f047c
Haileyesus Chekol <teamdevs7@gmail.com>
2024-04-24 10:41:48 +0100

    design hero section

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const list = document.querySelector(".nav-1")
 const menu = document.querySelector(".menu")
-
+const xBtn = document.querySelector(".x-btn")
 menu.addEventListener("click", () => {
+  xBtn.classList.add("x-btn-visible")
   list.classList.toggle("expand")
+  menu.classList.add("menu-visible")
+})
+
+xBtn.addEventListener("click", () => {
+  xBtn.classList.remove("x-btn-visible")
+  menu.classList.remove("menu-visible")
+  list.classList.remove("expand")
 })
